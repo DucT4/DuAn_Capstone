d6df9092a7f7d5da9ba52d123604976a6f7585eb
DungDao27 <tridung2156@gmail.com>
2023-04-27 17:26:46 +0700

    update-index4

--- js/index.js
+++ js/index.js
@@ -14,19 +14,21 @@
             var giay = manggiay[index];
 
             content +=`
-            <div class="col-md-4">
+            <div class="col-2">
                     <div class="card">
-                        <img src="${giay.image}" alt="...."/>
-
+                        <div class="pic">
+                          <img src="${giay.image}" alt="...."/>
+                        </div>
                         <div class="card-body">
-                            <div class="name-price">
-                            <h4 class="name">${giay.name}</h4>
-                                <p class="price">${giay.price}$</p>
-                            </div>
+                            <p class="name">${giay.name}</p>
+                            <p class="price">${giay.price}$</p>
+                            <div class="btn">
+                                <button class="custom-btn btn-buy">Buy now</button>
                             </div>
                         </div>
                     </div>
-                </div>`
+                </div>
+            </div>`
         }
         document.querySelector('#list-giay').innerHTML = content;
 
@@ -37,3 +39,112 @@
     })
 
 })();
+
+
+
+(function ()
+{
+    var promise = axios ({
+        url:'https://shop.cyberlearn.vn/api/Product/getProductByCategory?categoryId=ADIDAS',
+        method: 'GET',
+    }).then(function(res){
+
+        console.log(res.data);
+        var manggiay = res.data.content;
+        var content ='';
+        for(var index = 0; index < manggiay.length;index++)
+        {
+            //Mỗi lần duyệt lấy ra 1 object phim
+            var giay = manggiay[index];
+
+            content +=`
+            <div class="col-2">
+                    <div class="card">
+                        <div class="pic">
+                          <img src="${giay.image}" alt="...."/>
+                        </div>
+                        <div class="card-body">
+                            <p class="name">${giay.name}</p>
+                            <p class="price">${giay.price}$</p>
+                            <div class="btn">
+                                <button class="custom-btn btn-buy">Buy now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+        }
+        document.querySelector('#list-giay-adidas').innerHTML = content;
+
+    }).catch(function(err){
+
+        console.log(err.response.data)
+
+    })
+
+})();
+
+
+
+(function ()
+{
+    var promise = axios ({
+        url:'https://shop.cyberlearn.vn/api/Product/getProductByCategory?categoryId=NIKE',
+        method: 'GET',
+    }).then(function(res){
+
+        console.log(res.data);
+        var manggiay = res.data.content;
+        var content ='';
+        for(var index = 0; index < manggiay.length;index++)
+        {
+            //Mỗi lần duyệt lấy ra 1 object phim
+            var giay = manggiay[index];
+
+            content +=`
+            <div class="col-2">
+                    <div class="card">
+                        <div class="pic">
+                          <img src="${giay.image}" alt="...."/>
+                        </div>
+                        <div class="card-body">
+                            <p class="name">${giay.name}</p>
+                            <p class="price">${giay.price}$</p>
+                            <div class="btn">
+                                <button class="custom-btn btn-buy">Buy now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+        }
+        document.querySelector('#list-giay-nike').innerHTML = content;
+
+    }).catch(function(err){
+
+        console.log(err.response.data)
+
+    })
+
+})();
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
